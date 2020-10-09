9a48947f0627d6f8c67590f4d42ee2415db2fd2c
mikezalik <mike.zalik@gmail.com>
2020-10-09 10:22:11 -0700

    Adds server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
